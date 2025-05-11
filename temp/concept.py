9c21dd563cabb7a3246342f4929424ebd78508a0
S-DPR <thankyouformuch@naver.com>
2025-05-12 01:29:59 +0900

    feat: dsl 고민; 초기 변수 초기화를 지원

--- temp/concept.py
+++ temp/concept.py
@@ -1,6 +1,7 @@
 # $v는 이전에 지정한 변수
-# 입력포맷 : [ line ]
-# line : { variable: [ var.. ], repeat: $v, format: { ... }, type: (str) }
+# 입력포맷 : [ variable, line ]
+# 입력포맷의 variable은 초기init
+# line : { variable: [ var.. ], repeat: $v, format: { ... }, type: (str), line_repeat: $v }
 # line.type은 graph나 line..
 # var: { name: (str), type: (str), range: [int, int] }
 # format: 뭐 이런저런 옵션들... 뭐 separator나 sequence..
@@ -50,42 +51,56 @@ def create_variables(variable_format):
         variables[name] = (random.randint(start, end), types)
     return variables
 
-def process(lines):
-    result = []
-    variables = {}
-    for i in lines:
-        formats = i.get('format', {})
-        line_type = i.get('type', 'line')
-        sequence = formats.get('sequence', [])  # formats에서 sequence는 variable에 지정한 변수를 사용 가능하다 하자
-        separator = formats.get('separator', ' ')  # 그리고 세퍼레이터. 기본값은 스페이스
+def type_line_data_generator(variables, sequence):
+    line_data = []
+    TYPE_FUNCTION = {
+        'int': lambda val: val,
+        'char': lambda val: chr(val)
+    }
+    for seq in sequence:
+        if seq[0] == '$':
+            value, types = variables[seq[1:]]
+            line_data.append(TYPE_FUNCTION[types](value))
+        else:
+            line_data.append(seq)
+    return line_data
 
-        variables |= create_variables(i.get('variable', {}))
-        repeat_count = safe_eval(i.get('repeat', '1').replace("$", ""), variables)
-        current_line_data = []
-        for _ in range(repeat_count):
-            variables |= create_variables(i.get('variable', {}))
-            repeat_data = []
-            for seq in sequence:
-                if seq[0] == '$':
-                    value, types = variables[seq[1:]]
-                    if types == 'int':
-                        repeat_data.append(value)
-                    elif types == 'char':
-                        repeat_data.append(chr(value))
-                else:
-                    repeat_data.append(seq)
-            current_line_data.extend(repeat_data)
-        result.append(separator.join(map(str, current_line_data)))
+LINE_TYPE_FUNCTION = {
+    'line': type_line_data_generator,
+}
+
+def process(variable_format, lines):
+    result = []
+    variables = create_variables(variable_format)
+    for i in lines:
+        line_repeat = safe_eval(i.get('line_repeat', '1').replace("$", ""), variables)
+        for _ in range(line_repeat):
+            formats = i.get('format', {})
+            line_type = i.get('type', 'line')
+            sequence = formats.get('sequence', [])  # formats에서 sequence는 variable에 지정한 변수를 사용 가능하다 하자
+            separator = formats.get('separator', ' ')  # 그리고 세퍼레이터. 기본값은 스페이스
+            type_function = LINE_TYPE_FUNCTION[line_type]
+            variable_format = i.get('variable', {})
+
+            repeat_count = safe_eval(i.get('repeat', '1').replace("$", ""), variables)
+            current_line_data = []
+            for _ in range(repeat_count):
+                variables |= create_variables(variable_format)
+                current_line_data.extend(type_function(variables, sequence))
+            result.append(separator.join(map(str, current_line_data)))
     return result
 
 print(process([
+    { "name": "T", "type": "int", "range": [3, 3] }
+], [
   {
     "variable": [{ "name": "n", "type": "int", "range": [1, 1] }],
     "format": { "sequence": ["$n"] }
   },
   {
     "variable": [{ "name": "a", "type": "char", "range": [97, 122] }],
-    "repeat": "$n+3",
+    "repeat": "1",
+    "line_repeat": "$T",
     "format": { "sequence": ["$a"], "separator": "" }
   }
 ]
