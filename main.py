a4ac87f47d899d6fe446f73613f32e05c30cf83c
S-DPR <thankyouformuch@naver.com>
2025-05-17 01:05:48 +0900

    feat: 해당 기능 관련 API 엔드포인트 추가

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 
+from request.config_structs import TestcaseConfig
+from request.executor import process
+
 app = FastAPI()
 
 
@@ -11,3 +14,8 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.post("/create/testcase")
+async def create_testcase(testcase: TestcaseConfig):
+    return process(testcase)
