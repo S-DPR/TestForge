4d431928b734df9ab987c0716ffb0a426a07cb5d
S-DPR <thankyouformuch@naver.com>
2025-05-17 14:27:45 +0900

    del: 미사용 엔티티 제거

--- post/models.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.orm import Mapped, mapped_column
-from db.base import Base
-
-class Post(Base):
-    __tablename__ = "posts"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    title: Mapped[str] = mapped_column(nullable=False)
