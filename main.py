5378e021236f9e4e7fcb1b6debf5433da0860ca5
S-DPR <thankyouformuch@naver.com>
2025-05-17 17:57:20 +0900

    feat: unicorn.run의 host매개변수를 localhost로 수정

--- main.py
+++ main.py
@@ -38,4 +38,4 @@ async def create_testcase(testcase: TestcaseConfig):
     )
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="localhost", port=8000)
