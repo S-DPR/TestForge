ddd3233cda25cb7d0251bcf9e041b9c8c1cbc5b7
S-DPR <thankyouformuch@naver.com>
2025-05-19 16:15:00 +0900

    fix: save 매개변수 filepath -> folder

--- file_manager/tasks/file_manage.py
+++ file_manager/tasks/file_manage.py
@@ -6,9 +6,9 @@ from celery_app import celery_app
 def save_file_from_cfg_dict(cfg: dict):
     return save_file(**cfg)
 
-def save_file(filepath: str, content: str, ext: str = ".txt") -> dict[str, str]:
+def save_file(folder: str, content: str, ext: str = ".txt") -> dict[str, str]:
     filename = str(uuid.uuid4())
-    p = f"{filepath}/{filename}.{ext}"
+    p = f"{folder}/{filename}.{ext}"
     with open(p, "w") as f:
         f.write(content)
     return {
