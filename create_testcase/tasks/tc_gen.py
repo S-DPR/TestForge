713ef29a42340255fed5c24de3995d42cee0433e
S-DPR <thankyouformuch@naver.com>
2025-05-19 16:09:44 +0900

    feat: celery task 추가

---
+++ create_testcase/tasks/tc_gen.py
@@ -0,0 +1,23 @@
+from celery_app import celery_app
+from request.config_structs import TestcaseConfig
+from dacite import from_dict
+from request.executor import process
+
+
+@celery_app.task(name='tc_gen.tc_raw')
+def tc_gen_raw_from_cfg_dict(cfg: dict):
+    return tc_gen_raw(**cfg)
+
+def tc_gen_raw(testcaseConfig: dict):
+    return process(from_dict(data_class=TestcaseConfig, data=testcaseConfig))
+
+@celery_app.task(name='tc_gen.tc_upload')
+def tc_gen_upload_from_cfg_dict(cfg: dict):
+    return tc_gen_upload(**cfg)
+
+def tc_gen_upload(filepath: str, testcaseConfig: dict, ext: str):
+    return {
+        "filepath": filepath,
+        "content": process(from_dict(data_class=TestcaseConfig, data=testcaseConfig)),
+        "ext": ext
+    }
