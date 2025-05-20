5f6d1ec8bf62dc9872d5a1fd7f8fb73317669b9a
S-DPR <thankyouformuch@naver.com>
2025-05-20 14:20:48 +0900

    feat: kafka 연결 재시도를 위한 함수 추가

--- kafka_common/kafka_common/kafka_producer.py
+++ kafka_common/kafka_common/kafka_producer.py
@@ -1,8 +1,28 @@
-from confluent_kafka import Producer
+import time
+from threading import Lock
+from confluent_kafka import Producer, KafkaException
 from .config import KAFKA_BROKER_URL
 
-producer = Producer({'bootstrap.servers': KAFKA_BROKER_URL})
+_producer = None
+_lock = Lock()
+
+def get_kafka_producer():
+    global _producer
+    with _lock:
+        if _producer is not None:
+            return _producer
+        while True:
+            try:
+                _producer = Producer({'bootstrap.servers': KAFKA_BROKER_URL})
+                _producer.list_topics(timeout=5)
+                print("Kafka 연결 성공!")
+                break
+            except KafkaException as e:
+                print(f"Kafka 연결 실패: {e}, 3초 뒤 재시도")
+                time.sleep(3)
+        return _producer
 
 def send_message(topic: str, key: str, value: str):
-    producer.produce(topic, key=key, value=value)
-    producer.flush()
+    p = get_kafka_producer()
+    p.produce(topic, key=key, value=value)
+    p.flush()
