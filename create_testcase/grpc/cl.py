c136e44f944b46e114685c600cfcd87f8cb4745c
S-DPR <thankyouformuch@naver.com>
2025-05-21 15:15:21 +0900

    del: grpc 테스트용 엔드포인트 제거

---
+++ create_testcase/grpc/cl.py
@@ -0,0 +1,18 @@
+import grpc
+import v1_pb2_grpc, v1_pb2
+
+def run():
+    channel = grpc.insecure_channel("localhost:50051")  # gRPC 서버 주소
+    stub = v1_pb2_grpc.TCGenStub(channel)
+
+    request = v1_pb2.TcGenSaveReq(
+        folder="/app/scripts",
+        content="print('gRPC works!')",
+        ext="txt"
+    )
+
+    response = stub.TCGenSave(request)
+
+    return {"filepath": response.filepath}
+
+run()
