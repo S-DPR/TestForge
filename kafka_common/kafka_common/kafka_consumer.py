3f6b002429158bc10e4cda0767177f08e7667610
S-DPR <thankyouformuch@naver.com>
2025-05-21 10:34:15 +0900

    fix: 할당 기다리는 기능 제거

--- kafka_common/kafka_common/kafka_consumer.py
+++ kafka_common/kafka_common/kafka_consumer.py
@@ -3,7 +3,7 @@ from confluent_kafka import Consumer, KafkaException
 from .config import KAFKA_BROKER_URL
 
 
-def get_consumer(group_id: str, topic: str):
+def get_consumer(group_id: str, topic: list[str]):
     while True:
         try:
             _consumer = Consumer({
@@ -11,17 +11,9 @@ def get_consumer(group_id: str, topic: str):
                 'group.id': group_id,
                 'auto.offset.reset': 'earliest'
             })
-            _consumer.subscribe([topic])
-            _consumer.list_topics(timeout=5)  # 연결 테스트용
-            while not _consumer.assignment():
-                print("Waiting for partition assignment...", flush=True)
-                _consumer.poll(0)
-                _consumer.list_topics(timeout=5)
-                time.sleep(0.5)
-            print(f"Kafka Consumer 연결 성공! group_id={group_id}, topic={topic}", flush=True)
+            _consumer.subscribe(topic)
+            print(f"Kafka Consumer 연결 성공! group_id={group_id}, topic={','.join(topic)}", flush=True)
             return _consumer
         except KafkaException as e:
             print(f"Kafka Consumer 연결 실패: {e}, 3초 뒤 재시도", flush=True)
             time.sleep(3)
-
-    return None
