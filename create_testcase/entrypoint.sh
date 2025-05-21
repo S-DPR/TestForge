e80ee882660a6bae4ba964937799c5afc22f85bb
S-DPR <thankyouformuch@naver.com>
2025-05-21 15:47:19 +0900

    fix: 경로 수정

--- create_testcase/entrypoint.sh
+++ create_testcase/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python3 /app/grpc/compile_grpc.py create_testcase v1
+python3 /app/grpc/compile_grpc.py create_testcase
 python3 /app/create_testcase/grpc/server_v1.py &
 python3 /app/create_testcase/main.py
