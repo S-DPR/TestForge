6b3179fdb7f1a52fbc9ff7befab569639fa5fad6
S-DPR <thankyouformuch@naver.com>
2025-05-22 13:29:45 +0900

    feat: coderunner에 grpc, kafka 의존성 추가

---
+++ code_runner/grpc_internal/file_manager/client.py
@@ -0,0 +1,16 @@
+import grpc
+# from file_manager import v1_pb2_grpc, v1_pb2
+
+# def run(content, ext = "txt"):
+#     channel = grpc.insecure_channel("file-manager:50051")  # gRPC 서버 주소
+#     stub = v1_pb2_grpc.TCGenStub(channel)
+#
+#     request = v1_pb2.TcGenSaveReq(
+#         folder="/app/scripts",
+#         content=content,
+#         ext="txt"
+#     )
+#
+#     response = stub.TCGenSave(request)
+#
+#     return {"filepath": response.filepath}
