ef5d302a5725aecc407d2fcc7f01efafa7bc4eeb
S-DPR <thankyouformuch@naver.com>
2025-05-22 10:24:42 +0900

    fix: uvicorn으로 fastapi 실행하도록 수정

--- file_manager/entrypoint.sh
+++ file_manager/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python3 /app/grpc/compile_grpc.py file_manager
 python3 /app/file_manager/grpc/server_v1.py &
-python3 /app/file_manager/main.py
+uvicorn main:app --host 0.0.0.0 --port 8001
