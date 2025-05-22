9fa0f9ff8353a9547c3ef81c4157d0b398f43ef2
S-DPR <thankyouformuch@naver.com>
2025-05-22 16:11:11 +0900

    feat: testcase proto 추가

--- create_testcase/grpc_internal/server_v1.py
+++ create_testcase/grpc_internal/server_v1.py
@@ -1,23 +1,31 @@
-# import grpc_internal
-# from concurrent import futures
-# import v1_pb2, v1_pb2_grpc
-#
-#
-# class TCGenServicer(v1_pb2_grpc.TCGenServicer):
-#     def __init__(self):
-#         self.TcGenSaveRes = getattr(v1_pb2, 'TcGenSaveRes', None)
-#
-#     def TCGenSave(self, request, context):
-#         print("요청받음!")
-#         return self.TcGenSaveRes(filepath="hihi")
-#
-# def serve():
-#     server = grpc_internal.server(futures.ThreadPoolExecutor(max_workers=10))
-#     v1_pb2_grpc.add_TCGenServicer_to_server(TCGenServicer(), server)
-#     server.add_insecure_port('[::]:50051')
-#     server.start()
-#     print("CreateTestcase 서버 실행")
-#     server.wait_for_termination()
-#
-# if __name__ == '__main__':
-#     serve()
+import grpc
+import json
+from dacite import from_dict
+from concurrent import futures
+from create_testcase import v1_pb2, v1_pb2_grpc
+from request.config_structs import TestcaseConfig
+from request.executor import process
+
+
+class TestcaseServicer(v1_pb2_grpc.TestcaseServicer):
+    def __init__(self):
+        self.CreateTestcaseRes = getattr(v1_pb2, 'CreateTestcaseRes', None)
+
+    def TCGenSave(self, request, context):
+        format_ = request.format
+
+        format_dict = json.loads(format_)
+        testcase_config = from_dict(data_class=TestcaseConfig, data=format_dict)
+
+        return self.CreateTestcaseeRes(output=process(testcase_config))
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    v1_pb2_grpc.add_TestcaseServicer_to_server(TestcaseServicer(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    print("CreateTestcase 서버 실행")
+    server.wait_for_termination()
+
+if __name__ == '__main__':
+    serve()
