b7bd744dee3434c00e8f0016a18b2613e9b78b23
S-DPR <thankyouformuch@naver.com>
2025-05-22 16:14:31 +0900

    feat: test용 엔드포인트 추가

--- orchestrator/main.py
+++ orchestrator/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import service.code_service as code_service
 
 app = FastAPI()
 
@@ -11,3 +12,7 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+@app.post("runnn")
+async def runnn(format_, code1, code2, time_limit, repeat_count):
+    return code_service.run(format_, code1, code2, time_limit, repeat_count)
