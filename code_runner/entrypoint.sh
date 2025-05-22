f61023e3eca42065614c07c4962ebfdde0ebde9c
S-DPR <thankyouformuch@naver.com>
2025-05-22 14:10:53 +0900

    fix: 오타수정

--- code_runner/entrypoint.sh
+++ code_runner/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python3 /app/grpc/compile_grpc.py code_runner
-python3 /app/code_runner/grpc_ineternal/server_v1.py &
+python3 /app/code_runner/grpc_internal/server_v1.py &
 uvicorn main:app --host 0.0.0.0 --port 8002
