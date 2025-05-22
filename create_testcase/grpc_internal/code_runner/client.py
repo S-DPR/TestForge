6b15707c912b02018e961a7bfa09e171939496fd
S-DPR <thankyouformuch@naver.com>
2025-05-22 14:34:20 +0900

    feat: 테스트코드 추가

---
+++ create_testcase/grpc_internal/code_runner/client.py
@@ -0,0 +1,17 @@
+import grpc
+from code_runner import v1_pb2_grpc, v1_pb2
+
+def run(content, ext = "txt"):
+    channel = grpc.insecure_channel("code-runner:50051")  # gRPC 서버 주소
+    stub = v1_pb2_grpc.CodeRunnerStub(channel)
+
+    request = v1_pb2.ExecuteCodeReq(
+        code_path="/app/scripts/test.py",
+        language="python",
+        input_filepath=[],
+        timelimit=3
+    )
+
+    response = stub.ExecuteCode(request)
+
+    return {"filepath": response.filepath}
