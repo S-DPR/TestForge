17bab35e34b63321732e99ab7d409290976afa12
S-DPR <thankyouformuch@naver.com>
2025-05-23 11:24:38 +0900

    feat: 단일건으로 작동!

--- orchestrator/service/code_service.py
+++ orchestrator/service/code_service.py
@@ -4,10 +4,10 @@ from grpc_internal.file_manager import client as file_client
 
 def run(format_, code1, code2, time_limit, repeat_count):
     tc = tc_client.run(format_)
-    print(tc)
-    tc_path = file_client.run(tc.output)
-    *_, code1_name = file_client.run(code1).split("/")
-    *_, code2_name = file_client.run(code2).split("/")
+    print(tc, flush=True)
+    tc_path = file_client.run(tc['output'])
+    *_, code1_name = file_client.run(code1)['filepath'].split("/")
+    *_, code2_name = file_client.run(code2)['filepath'].split("/")
     code1_output_path = code_client.run(code1_name, "python", tc_path, time_limit)
     code2_output_path = code_client.run(code2_name, "python", tc_path, time_limit)
     return {
