768e0b8e7b2d5cc3ba2c4523629a3b7a0d4192a8
S-DPR <thankyouformuch@naver.com>
2025-05-28 13:17:41 +0900

    feat: --reload 옵션 추가

--- create_testcase/entrypoint.sh
+++ create_testcase/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python3 /app/grpc/compile_grpc.py create_testcase
 python3 /app/create_testcase/grpc_internal/server_v1.py &
-uvicorn main:app --host 0.0.0.0 --port 8000
+uvicorn main:app --host 0.0.0.0 --port 8000 --reload
--- orchestrator/entrypoint.sh
+++ orchestrator/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 python3 /app/grpc/compile_grpc.py orchestrator
-uvicorn main:app --host 0.0.0.0 --port 8003
+uvicorn main:app --host 0.0.0.0 --port 8003 --reload
