090e03de77ded99b8cbe24f4778d2ca68ba520cc
S-DPR <thankyouformuch@naver.com>
2025-05-28 16:22:25 +0900

    feat: client channel 전역변수화

--- orchestrator/grpc_internal/code_runner/client.py
+++ orchestrator/grpc_internal/code_runner/client.py
@@ -1,8 +1,9 @@
 import grpc
 from code_runner import v1_pb2_grpc, v1_pb2
 
+channel = grpc.insecure_channel("code-runner:50051")  # gRPC 서버 주소
+
 def execute_code(account_id, code_filename, language, input_filepath, output_filepath, timelimit):
-    channel = grpc.insecure_channel("code-runner:50051")  # gRPC 서버 주소
     stub = v1_pb2_grpc.CodeRunnerStub(channel)
 
     request = v1_pb2.ExecuteCodeReq(
