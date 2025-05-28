32fe457a18d4cfdf76b617dd4789524a1f8809d1
S-DPR <thankyouformuch@naver.com>
2025-05-28 15:18:23 +0900

    fix: db setup 경로 변경

--- create_testcase/entrypoint.sh
+++ create_testcase/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python3 /app/grpc/compile_grpc.py create_testcase
 python3 /app/create_testcase/grpc_internal/server_v1.py &
-python3 /app/code_runner/database_setup.py
+python3 /app/create_testcase/database_setup.py
 uvicorn main:app --host 0.0.0.0 --port 8000 --reload
