73ccdaf57c039632d4c4c4380a20f0c077e95170
S-DPR <thankyouformuch@naver.com>
2025-05-28 14:30:00 +0900

    feat: db setup 코드 호출 추가

--- code_runner/entrypoint.sh
+++ code_runner/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 python3 /app/grpc/compile_grpc.py code_runner
 python3 /app/code_runner/grpc_internal/server_v1.py &
+python3 /app/code_runner/database_setup.py
 uvicorn main:app --host 0.0.0.0 --port 8002 --reload
