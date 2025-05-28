f6f248ab1daffafc6a152f69633a2570e47dab54
S-DPR <thankyouformuch@naver.com>
2025-05-28 15:18:08 +0900

    fix: 매개변수 순서 변경

--- create_testcase/grpc_internal/server_v1.py
+++ create_testcase/grpc_internal/server_v1.py
@@ -21,7 +21,7 @@ class TestcaseServicer(v1_pb2_grpc.TestcaseServicer):
 
         with ProcessPoolExecutor(max_workers=4) as executor:
             testcase_config = from_dict(data_class=TestcaseConfig, data=format_dict)
-            futures = [executor.submit(account_id, process, testcase_config) for _ in range(repeat_count)]
+            futures = [executor.submit(process, account_id, testcase_config) for _ in range(repeat_count)]
             for f in as_completed(futures):
                 yield v1_pb2.CreateTestcaseRes(output=f.result())
 
