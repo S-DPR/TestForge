e0e6e5f1994f73a19ff541755c2ef2ac49c6f42f
S-DPR <thankyouformuch@naver.com>
2025-05-30 01:53:53 +0900

    feat: 모듈로 실행 추가

--- auth/entrypoint.sh
+++ auth/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python3 /app/grpc/compile_grpc.py auth
-python3 /app/auth/grpc_internal/server_v1.py &
+python3 -m /app/auth/grpc_internal/server_v1.py &
 python /app/auth/manage.py makemigrations
 python /app/auth/manage.py migrate
 python /app/auth/manage.py runserver 0.0.0.0:9000
