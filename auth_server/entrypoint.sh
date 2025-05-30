cb5643151c67e07cc6d72f19bdf5556b5d9e1fec
S-DPR <thankyouformuch@naver.com>
2025-05-30 11:25:07 +0900

    fix: 파이썬 실행 경로 에러 수정

--- auth_server/entrypoint.sh
+++ auth_server/entrypoint.sh
@@ -2,5 +2,5 @@
 python3 /app/grpc/compile_grpc.py auth_server
 python /app/auth_server/manage.py makemigrations
 python /app/auth_server/manage.py migrate
-python3 /app/auth_server/grpc_internal.server_v1 &
+python3 /app/auth_server/grpc_internal/server_v1.py &
 python /app/auth_server/manage.py runserver 0.0.0.0:9000
