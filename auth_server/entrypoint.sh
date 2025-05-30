2933ee96c1014a0a98605c1e76eca7e625e72025
S-DPR <thankyouformuch@naver.com>
2025-05-30 11:15:19 +0900

    test: account 마이그레이션이 안되는게 이거때문인가싶어 -m으로 롤백

--- auth_server/entrypoint.sh
+++ auth_server/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 python3 /app/grpc/compile_grpc.py auth_server
+
 cd /app/auth_server
 python3 -m grpc_internal.server_v1 &
+
+cd /
 python /app/auth_server/manage.py makemigrations
 python /app/auth_server/manage.py migrate
 python /app/auth_server/manage.py runserver 0.0.0.0:9000
