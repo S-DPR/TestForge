7b84fad8b9a81d9fdca4cd23cea788bcadf5ab8a
S-DPR <thankyouformuch@naver.com>
2025-05-30 09:50:58 +0900

    fix: auth -> auth_server로 인증서버 이름 변경

---
+++ auth_server/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+python3 /app/grpc/compile_grpc.py auth_server
+python3 -m grpc_internal.server_v1 &
+python /app/auth_server/manage.py makemigrations
+python /app/auth_server/manage.py migrate
+python /app/auth_server/manage.py runserver 0.0.0.0:9000
