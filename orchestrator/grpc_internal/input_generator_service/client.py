b390609861c97ad7dcfa1ac42249e6b341f9ac46
S-DPR <thankyouformuch@naver.com>
2025-06-02 15:00:18 +0900

    feat: canceller로 종료여부 판단 로직 추가

--- orchestrator/grpc_internal/input_generator_service/client.py
+++ orchestrator/grpc_internal/input_generator_service/client.py
@@ -2,7 +2,7 @@ import grpc.aio
 import json
 from input_generator_service import v1_pb2_grpc, v1_pb2
 
-async def testcase_generate(account_id, format_, repeat_count, context):
+async def testcase_generate(account_id, format_, repeat_count, canceller):
     async with grpc.aio.insecure_channel("input-generator-service:50051") as channel:
         stub = v1_pb2_grpc.TestcaseStub(channel)
 
@@ -14,7 +14,7 @@ async def testcase_generate(account_id, format_, repeat_count, context):
 
         call = stub.CreateTestcase(request)
         async for response in call:
-            if context.cancelled():
+            if canceller.is_cancelled():
                 call.cancel()
                 break
             yield {"output": response.output}
