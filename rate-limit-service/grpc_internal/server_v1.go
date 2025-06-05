641a69eb3c406ff86b37db649a44d4d8b4643476
S-DPR <thankyouformuch@naver.com>
2025-06-05 15:21:54 +0900

    feat: grpcServer

--- rate-limit-service/grpc_internal/server_v1.go
+++ rate-limit-service/grpc_internal/server_v1.go
@@ -3,15 +3,34 @@ package grpc_internal
 import (
 	"context"
 	"github.com/redis/go-redis/v9"
+	"log"
+	pb "rate-limit-service/grpc_internal/rate-limit-service"
 	"rate-limit-service/internal/service"
 )
 
-func serve() {
+var testExecutionLimit service.TestExecutionLimitService
+
+type server struct {
+	pb.UnimplementedFileServer
+}
+
+func (s *server) FileSave(ctx context.Context, req *pb.TestExecutionLimitReq) (*pb.TestExecutionLimitRes, error) {
+	log.Printf("Received request: account_id=%s, count=%d", req.AccountId, req.Count)
+
+	isLimit, accCount := testExecutionLimit.IsRateLimited(req.AccountId, req.Count)
+
+	return &pb.TestExecutionLimitRes{
+		IsLimit:  isLimit,
+		AccCount: accCount,
+	}, nil
+}
+
+func Serve() {
 	ctx := context.Background()
 	rdb := redis.NewClient(&redis.Options{
 		Addr:     "redis:6379",
 		Password: "",
 		DB:       0,
 	})
-	client := service.NewTestExecutionLimitService(ctx, rdb)
+	testExecutionLimit = service.NewTestExecutionLimitService(ctx, rdb)
 }
