f2847cad84f01ac0a108adf8c46302dabdab59b4
S-DPR <thankyouformuch@naver.com>
2025-06-05 15:36:26 +0900

    fix: grpc server 추가

--- rate-limit-service/grpc_internal/server_v1.go
+++ rate-limit-service/grpc_internal/server_v1.go
@@ -3,7 +3,9 @@ package grpc_internal
 import (
 	"context"
 	"github.com/redis/go-redis/v9"
+	"google.golang.org/grpc"
 	"log"
+	"net"
 	pb "rate-limit-service/grpc_internal/rate_limit_service"
 	"rate-limit-service/internal/service"
 )
@@ -33,4 +35,17 @@ func Serve() {
 		DB:       0,
 	})
 	testExecutionLimit = service.NewTestExecutionLimitService(ctx, rdb)
+
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterWhateverServer(s, &server{})
+
+	log.Println("gRPC server listening on :50051")
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
