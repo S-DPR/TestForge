4f652b77d5011bfe4f673a26cb22d3fe93854e4f
S-DPR <thankyouformuch@naver.com>
2025-06-07 23:07:52 +0900

    del: path에 oauth 흔적 제거

--- auth_server/auth_server/urls.py
+++ auth_server/auth_server/urls.py
@@ -19,5 +19,5 @@ Including another URLconf
 from django.urls import include, path
 
 urlpatterns = [
-    path('auth/', include('social_django.urls', namespace='social'))
+    # path('auth/', include('social_django.urls', namespace='social'))
 ]
