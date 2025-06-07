abb3a9e467f95f73a666f7b23b4200db88f6dab7
S-DPR <thankyouformuch@naver.com>
2025-06-08 02:39:17 +0900

    fix: grpc 서버 켤 때 백그라운드에서 키는 부분 제거

--- execution-service/entrypoint.sh
+++ execution-service/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python3 /app/grpc/compile_grpc.py execution-service
-python3 /app/execution-service/grpc_internal/server_v1.py &
 python3 /app/execution-service/database_setup.py
+python3 /app/execution-service/grpc_internal/server_v1.py
 #uvicorn main:app --host 0.0.0.0 --port 8002 --reload
