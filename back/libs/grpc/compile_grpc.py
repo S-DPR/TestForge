060233f825d656384bd5f1ab8cd715fd6d453c90
S-DPR <thankyouformuch@naver.com>
2025-06-30 10:07:30 +0900

    feat: Timestamp 사용할 수 있도록 import추가

--- back/libs/grpc/compile_grpc.py
+++ back/libs/grpc/compile_grpc.py
@@ -2,6 +2,7 @@ from grpc_tools import protoc
 import platform
 import sys
 import glob
+import importlib.resources
 
 prefix = "/app/grpc"
 _, folder, *_ = *sys.argv, 0
@@ -13,13 +14,14 @@ proto_files = glob.glob(f'{prefix}/**/*.proto', recursive=True)
 
 print(proto_files)
 
-args = [
-    "",
-    f"-I=/app/grpc",
-    f"--python_out=/app/{folder}/grpc_internal",
-    f"--grpc_python_out=/app/{folder}/grpc_internal",
-    *proto_files
-]
+with importlib.resources.path("grpc_tools", "_proto") as proto_include:
+    args = [
+        "",
+        f"-I=/app/grpc",
+        f"--python_out=/app/{folder}/grpc_internal",
+        f"--grpc_python_out=/app/{folder}/grpc_internal",
+        *proto_files
+    ]
 if platform.system() == "Windows":
     args = [
         "",
